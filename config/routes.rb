f1af7341e984c0af4e9e0c8a0122674be5ba7065
James Rae <jrae151515@yahoo.co.uk>
2022-11-30 18:22:24 +0000

    created model and specified basic routes, filled controller with 7 RESTful actions. Then created basic views for the controller actions. Manually tested and all working as expected

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'kittens/index'
+  get 'kittens/new'
+  get 'kittens/create'
+  get 'kittens/update'
+  get 'kittens/edit'
+  get 'kittens/destroy'
+  get 'kittens/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "kittens#index"
+  resources :kittens
 end
